3261afad16bb9f65f0b162b8f38e03764914c523
excited4trave <162991131+excited4trave@users.noreply.github.com>
2024-03-11 12:52:22 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+var count=0;
+function increase_function(){
+
+     count+=1;
+document.getElementById("counter_value").innerHTML=count;
+ }
+ 
+ function decrease_function(){
+         count-=1;
+
+document.getElementById("counter_value").innerHTML=count;
+
+ }
